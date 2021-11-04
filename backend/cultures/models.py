c9c84f429dae89c8ce7697438ebb0b2f1cad51d4
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2021-11-04 18:23:55 +0300

    Create Django project. Create cultures app

---
+++ backend/cultures/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class Culture(models.Model):
+    class Origins(models.TextChoices):
+        SEED = 'seed', 'Seed'
+        CLONE = 'clone', 'Clone'
+
+    class Rooms(models.TextChoices):
+        CLONING = 'cloning', 'Cloning'
+        VEGETATION = 'vegetation', 'Vegetation'
+        FLOWERING = 'flowering', 'Flowering'
+        DRYING = 'drying', 'Drying'
+        STORAGE = 'storage', 'Storage'
+
+    class DiscardReasons(models.TextChoices):
+        MALE = 'male', 'Male'
+        PEST = 'pest', 'Pest'
+
+    genetic_name = models.CharField('Genetic name', max_length=200)
+    origin = models.CharField(
+        'Origin', max_length=20, choices=Origins.choices, default=Origins.SEED
+    )
+    start_date = models.DateField()
+    flowering_date = models.DateField(null=True)
+    harvest_date = models.DateField(null=True)
+    harvest_weight = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    drying_date = models.DateField(null=True)
+    drying_weight = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    storage_date = models.DateField(null=True)
+    discarded = models.BooleanField(default=False)
+    discard_reason = models.CharField(
+        'Discard Reason', max_length=4, choices=DiscardReasons.choices, null=True
+    )
+
+
